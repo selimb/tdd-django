fb42967089e691c99ad826dfaad9baf4135818a1
Selim Belhaouane <selim.belhaouane@gmail.com>
2020-04-19 16:00:53 -0400

    new URL for adding to existing list. FT passes

--- lists/views.py
+++ lists/views.py
@@ -11,9 +11,15 @@ def home_page(request: HttpRequest):
 def new_list(request: HttpRequest):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=list_)
-    return redirect("/lists/the-only-list-in-the-world/")
+    return redirect(f"/lists/{list_.id}/")
 
 
-def view_list(request: HttpRequest):
-    items = Item.objects.all()
-    return render(request, "list.html", {"items": items})
+def view_list(request: HttpRequest, list_id: str):
+    list_ = List.objects.get(id=list_id)
+    return render(request, "list.html", {"list": list_})
+
+
+def add_item(request: HttpRequest, list_id: str):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=list_)
+    return redirect(f"/lists/{list_.id}/")
--- superlists/settings.py
+++ superlists/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from typing import List
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = "g#c6!@0do0w7zp-^#q)*z^d5_6r^4soj9zoo&&^#kwzu7g2f=9"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
