b8393ebd25b6a5e56b35a951b17b3e5727ff9086
Selim Belhaouane <selim.belhaouane@gmail.com>
2020-04-19 14:54:59 -0400

    Add specific endpoint for creating lists

--- lists/views.py
+++ lists/views.py
@@ -5,10 +5,12 @@ from lists.models import Item
 
 
 def home_page(request: HttpRequest):
-    if request.method == "POST":
-        Item.objects.create(text=request.POST["item_text"])
-        return redirect("/lists/the-only-list-in-the-world/")
     return render(request, "home.html")
+
+
+def new_list(request: HttpRequest):
+    Item.objects.create(text=request.POST["item_text"])
+    return redirect("/lists/the-only-list-in-the-world/")
 
 
 def view_list(request: HttpRequest):
