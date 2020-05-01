dded31e2465710b7bde0a6d9f7be23e8666c1af6
Selim Belhaouane <selim.belhaouane@gmail.com>
2020-04-30 22:21:57 -0400

    fabric file to automate deployment

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,7 @@ import random
 from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
 
-REPO_URL = "https://github.com/hjwp/book-example.git"
+REPO_URL = "https://github.com/selimb/tdd-django.git"
 
 
 def deploy():
@@ -14,3 +14,29 @@ def deploy():
         _create_or_update_dotenv()
         _update_static_files()
         _update_database()
+
+
+def _get_latest_source():
+    if exists(".git"):
+        run("git fetch")
+    else:
+        run(f"git clone {REPO_URL} .")
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f"git reset --hard {current_commit}")
+
+
+def _update_virtualenv():
+    if not exists(".venv/bin/pip"):
+        run(f"python3.6 -m venv .venv")
+    run("./.venv/bin/pip install -r requirements.txt")
+
+
+def _create_or_update_dotenv():
+    append(".env", "DJANGO_DEBUG_FALSE=y")
+    append(".env", f"SITENAME={env.host}")
+    current_contents = run("cat .env")
+    if "DJANGO_SECRET_KEY" not in current_contents:
+        new_secret = "".join(
+            random.SystemRandom().choices("abcdefghijklmnopqrstuvwxyz0123456789", k=50)
+        )
+        append(".env", f"DJANGO_SECRET_KEY={new_secret}")
