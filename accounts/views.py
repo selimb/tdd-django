4dabaa8ab6a50322b3ef8ab067bef5e8ea2d9bf2
Selim Belhaouane <selim.belhaouane@gmail.com>
2020-05-09 12:20:35 -0400

    wire up basic login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import redirect
+
+
+def send_login_email(request: HttpRequest):
+    return redirect("/")
+
 
 # Create your views here.
